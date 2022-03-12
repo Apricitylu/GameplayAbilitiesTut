18bc8e69962fadc97d6305c592c4d8d75a7de358
Apricitylu <1376961154@qq.com>
2022-03-13 01:04:16 +0800

    创建第三人称模板+勾选GAS插件

---
+++ Source/GameplayAbilitiesTut/GameplayAbilitiesTutGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameplayAbilitiesTutGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGameplayAbilitiesTutGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGameplayAbilitiesTutGameMode();
+};
+
+
+
